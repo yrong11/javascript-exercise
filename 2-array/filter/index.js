9e10b65ede93dde3e84031c00f842c2963313fa1
yurong <rong.yu@thoughtworks.com>
2020-08-10 16:09:37 +0800

    [yurong] complete array filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,13 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(item => item % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter(function(item, index, collection) {
+    return collection.indexOf(item, 0) == index;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
