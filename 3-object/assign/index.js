ac2f0a892a0636fbd2d783007ecf6ca24f1bf955
yurong <rong.yu@thoughtworks.com>
2020-08-10 16:31:08 +0800

    [yurong] complete object assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,7 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const expect = { serialNumber: '12345' };
+  Object.assign(expect, source);
+  expect.properties.status = 'processed';
+  return expect;
 }
