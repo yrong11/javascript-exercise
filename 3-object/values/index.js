97a1510c426da090249fcd3e5cf16aab6cfcab6a
yurong <rong.yu@thoughtworks.com>
2020-08-10 19:42:08 +0800

    [yurong] complete object values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
+import { parse } from '@babel/core';
+
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  Object.values(source).forEach(item => (sum += parseInt(item)));
+  return sum;
 }
