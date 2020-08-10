b3fb0105f144739c06956603cb64bd80ad101ff9
yurong <rong.yu@thoughtworks.com>
2020-08-10 20:15:31 +0800

    [yurong] complete class practice 3

--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,8 @@
-// TODO 14: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  // eslint-disable-next-line class-methods-use-this
+  introduce() {
+    return 'I am a Worker. I have a job.';
+  }
+}
