6716404a9ea6d55203f36e8a54ee5e58f6045f7b
yurong <rong.yu@thoughtworks.com>
2020-08-10 16:22:34 +0800

    [yurong] complete array map

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,5 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const aCode = 'a'.charCodeAt(0);
+  return collection.map(item => String.fromCharCode(aCode + item - 1));
 }
