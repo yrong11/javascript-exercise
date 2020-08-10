95b520f0843cab525e71da0bc44d61cac5759ca3
yurong <rong.yu@thoughtworks.com>
2020-08-10 20:23:21 +0800

    [yurong] complete class practice 5

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    let s = `${super.introduce()} I am a Teacher.`;
+    if (!this.klass) s += ` I teach No Class.`;
+    else s += ` I teach Class ${this.klass}.`;
+    return s;
+  }
+}
